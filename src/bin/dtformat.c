15f181c9591c20e5e281df4f7f146984918f0f41
Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2023-03-07 19:36:36 +0100

    `to_int`, `extract_guid`, `get_volume_id`, `get_object_id`

--- src/bin/dtformat.c
+++ src/bin/dtformat.c
@@ -6,6 +6,15 @@
 SQLITE_EXTENSION_INIT1
 
 
+static void to_int(
+  sqlite3_context *pCtx,
+  int argc,
+  sqlite3_value **argv
+){
+  sqlite3_int64  *ptr = sqlite3_value_blob(argv[0]);
+  sqlite3_result_int64(pCtx, *ptr);
+}
+
 static void datetime_format(
   sqlite3_context *pCtx,
   int argc,
@@ -19,15 +28,47 @@ static void datetime_format(
     int     size = 30;
     char  * buf = malloc(size);
     size_t  mult = 10000000;
-    double  n;
-    size_t  fract = modf((double)v / mult, &n) * mult;
+    size_t  fract = v % mult;
 
     snprintf (buf, size,"%04d-%02d-%02dT%02d:%02d:%02d.%07dZ", 
               st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond, fract);
     sqlite3_result_text(pCtx, buf, size, free);
   } else {
-    sqlite3_result_error(pCtx, "Error", -1);
+    sqlite3_result_error(pCtx, "datetime_format error", -1);
   }
+}
+
+static void extract_guid(sqlite3_context *pCtx, sqlite3_value **argv, const char *pat) {
+  const char *str = (const char*)sqlite3_value_text(argv[0]);
+  if (str == NULL)
+    return;
+
+  const char *start = strstr(str, pat) + strlen(pat);
+
+  if (start) {
+    const char *end = strchr(start, '}');
+    size_t      size = end - start + 1;
+    char       *text = (char *)malloc(size + 1);
+    
+    strncpy(text, start, size);
+    sqlite3_result_text(pCtx, text, size, free);
+  } 
+}
+
+static void get_volume_id(
+  sqlite3_context *pCtx,
+  int argc,
+  sqlite3_value **argv
+){
+  extract_guid(pCtx, argv, "VolumeId=");
+}
+
+static void get_object_id(
+  sqlite3_context *pCtx,
+  int argc,
+  sqlite3_value **argv
+){
+  extract_guid(pCtx, argv, "ObjectId=");
 }
 
 #ifdef _WIN32
@@ -40,7 +81,14 @@ int sqlite3_dtformat_init(
 ){
   int rc = SQLITE_OK;
   SQLITE_EXTENSION_INIT2(pApi);
+  rc = sqlite3_create_function(db, "to_int", 1, SQLITE_UTF8, 0,
+                               to_int, 0, 0);
   rc = sqlite3_create_function(db, "datetime_format", 1, SQLITE_UTF8, 0,
                                datetime_format, 0, 0);
+  rc = sqlite3_create_function(db, "get_volume_id", 1, SQLITE_UTF8, 0,
+                               get_volume_id, 0, 0);
+  rc = sqlite3_create_function(db, "get_object_id", 1, SQLITE_UTF8, 0,
+                               get_object_id, 0, 0);
+
   return rc;
 }
