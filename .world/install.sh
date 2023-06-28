2924be5ec79d474f21c1bb634fed008a2e0850e6
Leo Sukharnikov <leonid.sukharnikov@aon.com>
2023-06-28 13:36:42 -0400

    R! - no need to use if statement becaus of EXE_EXT var

--- .world/install.sh
+++ .world/install.sh
@@ -8,14 +8,4 @@ if [[ "$Linkage" == 'static' ]]; then
   exit
 fi
 
-cargo build -r
-
-if [ "$Target" = 'linux' ]; then
-
-  cp $BASEDIR/target/release/sidr $INSTALL/bin
-
-elif [ "$Target" = 'windows' ]; then
-
-  cp $BASEDIR/target/release/sidr.exe $INSTALL/bin
-
-fi
+cp $BASEDIR/target/release/sidr${EXE_EXT} $INSTALL/bin
