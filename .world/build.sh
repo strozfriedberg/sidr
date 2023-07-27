47e98628dc31e96e098240886b64bbd116ad152c
Leo Sukharnikov <leonid.sukharnikov@aon.com>
2023-07-27 18:32:36 -0400

    F - building sidr for windows

--- .world/build.sh
+++ .world/build.sh
@@ -9,5 +9,15 @@ fi
 BASEDIR=$(pwd)
 WSA_TEST_DB_PATH=$BASEDIR/tests/testdata WSA_TEST_CONFIGURATION_PATH=$BASEDIR/src/bin/test_reports_cfg.yaml cargo test
 
-cargo build -r
+if [ "$Target" = 'linux' ]; then
+
+  cargo build -r
+
+elif [ "$Target" = 'windows' ]; then
+
+  rustup target add x86_64-pc-windows-gnu
+  cargo build -r --target x86_64-pc-windows-gnu
+
+fi
+
 ls /home/builder/make_world/world/sidr/target/release/
