587f77e6454ac7376fc36edf73dd9d44519618d1
Kim Stone <kimberly.stone@aon.com>
2023-08-04 07:52:23 -0700

    B - address warnings; update dirty DB warning text

--- src/ese.rs
+++ src/ese.rs
@@ -165,9 +165,9 @@ pub fn ese_generate_report(f: &Path, report_prod: &ReportProducer) -> Result<(),
 
     if jdb.get_database_state() != DbState::CleanShutdown {
         eprintln!("WARNING: The database state is not clean.");
-        eprintln!("Please use EseUtil which helps check the status (/MH) of a database and perform a soft (/R) or hard (/P) recovery");
+        eprintln!("Please use EseUtil which helps check the status (/MH) of a database and perform a soft (/R) or hard (/P) recovery,");
         eprintln!("or system32/esentutl for repair (/p).");
-        eprintln!("Results could be inaccurate and unstable work (even crash) is possible.\n");
+        eprintln!("Processing a dirty database may generate inaccurate and/or incomplete results.\n");
     }
 
     let t = "SystemIndex_PropertyStore";
@@ -438,16 +438,15 @@ fn ese_activity_history_record(
 }
 
 mod tests {
-    use std::{
-        fs,
-        io::Read,
-        path::PathBuf,
-        process::{Command, Stdio},
-    };
-    use tempdir::TempDir;
-
     #[test]
     fn warn_dirty() {
+        use std::{
+            fs,
+            path::PathBuf,
+            process::Command,
+        };
+        use tempdir::TempDir;
+
         let test_dir =
             TempDir::new("test_warn_dirty").unwrap_or_else(|e| panic!("TempDir::new failed: {e}"));
         let src = "tests/testdata/Windows.edb";
@@ -462,7 +461,7 @@ mod tests {
         });
         let sidr_bin = bin_root.join("sidr");
         let work_dir = test_dir.path().display().to_string();
-        let mut output = Command::new(sidr_bin)
+        let output = Command::new(sidr_bin)
             .args(["--outdir", &work_dir, &work_dir])
             .output()
             .expect("failed to execute process");
